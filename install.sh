6be24d06347234f2845c1f6a4f84d2dd117d7633
paulie <paul.adam@live.se>
2017-05-01 20:00:26 +0200

    Update installation script to download and run vundle plugin manager for vim"

--- install.sh
+++ install.sh
@@ -12,20 +12,31 @@ files="bashrc vimrc vim tmux.conf"    # list of files/folders to symlink in home
 
 ##########
 
-# create dotfiles_old in homedir
+# Create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
 echo "...done"
 
-# change to the dotfiles directory
+# Change to the dotfiles directory
 echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+# Move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
+
+# Install vundle plugin manager for vim
+echo "Cloning vundle..."
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo "...done"
+
+# Install vim plugins
+echo "Installing vim plugins"
+vim +PluginInstall +qall
+echo "...done"
+
